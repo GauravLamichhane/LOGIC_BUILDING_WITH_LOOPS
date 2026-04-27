539865405f8ef1ea7bfe673034eb417567d442e0
Gaurav <gaurav.lamichhane02@gmail.com>
2026-04-27 22:53:06 +0545

    mathematical series

---
+++ mathematical_series.py
@@ -0,0 +1,43 @@
+# Phase 6 : Mathematical Series
+
+# 1. Find and print the sum of the first n natural numbers.
+
+# n = int(input("Enter the number: "))
+# summ = (n * (n + 1)) / 2
+# print(summ)
+
+# 2. Find and print the sum of the first n even numbers.
+# n = int(input("Enter the number: "))
+# summ = 0
+# for i in range(1, n + 1):
+#     summ += 2 * i
+# print(summ)
+
+# 3. Find and print the sum of the first n odd numbers.
+# n = int(input("Enter the number: "))
+# summ = 0
+# for i in range(1, n + 1):
+#     summ += 2 * i - 1
+# print(summ)
+
+# 4. Print the first n terms of an arithmetic progression for the given first term and
+# common difference.
+# 5. Print the first n terms of a geometric progression for the given first term and
+# common ratio.
+# 6. Print the Fibonacci series up to the required number of terms.
+# 7. Find and print the sum of the Fibonacci series up to the required number of terms.
+# 8. Calculate and print the value of the series
+# 1² + 2² + 3² + … + n².
+# 9. Calculate and print the value of the series
+# 1³ + 2³ + 3³ + … + n³.
+# 10. Calculate and print the value of the series
+# 1 + 1/2 + 1/3 + … + 1/n.
+# 11. Print the series of powers of two:
+# 1 + 2 + 4 + 8 + … + 2ⁿ.
+# 12. Calculate and print the value of the series
+# 1! + 2! + 3! + … + n!.
+# 13. Calculate and print the value of the series
+# 1 + x + x² + x³ + … + xⁿ.
+# 14. Calculate and print the value of the series
+# x − x²/2! + x³/3! − x⁴/4! + ….
+# 15. Check whether the given number is a Strong number, where the number equals the sum of factorial of its digits.
